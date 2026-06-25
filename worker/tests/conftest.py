3efe655fc3e8de40481eb997e435ec88ecdb5db6
bestankit-sudo <bestankit@gmail.com>
2026-06-25 18:50:19 +0530

    test: add pytest scaffold for the worker package

---
+++ worker/tests/conftest.py
@@ -0,0 +1,2 @@
+"""Shared test fixtures. `pythonpath = ["src"]` in pyproject makes
+`plaud_worker` importable; nothing else is needed here yet."""
---
+++ worker/tests/test_scaffold.py
@@ -0,0 +1,2 @@
+def test_plaud_worker_importable():
+    import plaud_worker  # noqa: F401
